8759fbd3dea6099a79ffa77647b6a278fa61b9d9
Kazusa Okuda <klaNath@users.noreply.github.com>
2015-04-19 15:40:46 +0900

    add comment operator.v

--- operator.v
+++ operator.v
@@ -21,7 +21,7 @@ module operator(
           reg   [7:0]   p_synth_ctrl;
           reg           wreq_r, wr_now;
 
-  initial add_1 <= 19224;
+  initial add_1 <= 19224;                           //default frequency
 
   assign  data_out = accum_1[15:0];
   assign  phase = phase_1;
@@ -41,18 +41,18 @@ module operator(
       begin
         p_synth_ctrl <= synth_ctrl;
         case(synth_ctrl)
-          8'b00000001 : phase_1 <= add_1;
-          8'b10000001 : wreq_r <= 1;
-          8'b01000001 : begin
+          8'b00000001 : phase_1 <= add_1;             //step phase
+          8'b10000001 : wreq_r <= 1;                  //write req to fifo(one shot)
+          8'b01000001 : begin                         //write synth_data to lower add_1
                           add_1[7:0] <= synth_data;
-                          wr_now <= 1;
+                          wr_now <= 1;                //wr_now is unused yet
                         end
-          8'b00010001 : add_1[15:8] <= synth_data;
-          8'b01010001 : begin
+          8'b00010001 : add_1[15:8] <= synth_data;    //write synth_data to middle of add_1
+          8'b01010001 : begin                         //write  synth_data to upper of add_1
                           add_1[23:16] <= synth_data;
                           wr_now <= 0;
                         end
-          8'b00100000 : begin
+          8'b00100000 : begin                         //reset command
                           phase_1 <= 0;
                           accum_1 <= 0;
                           add_1 <= 0;
@@ -62,7 +62,7 @@ module operator(
           default     : phase_1 <= phase_1;
         endcase
         case(p_synth_ctrl)
-          8'b00000001 : accum_1 <= sine_out;
+          8'b00000001 : accum_1 <= sine_out;          //store output data to accum_1
           8'b10000001 : wreq_r <= 0;
         endcase
       end
